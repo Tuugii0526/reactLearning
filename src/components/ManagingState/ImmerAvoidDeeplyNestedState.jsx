757759cdd20737150b88a8926c0227e71a3f92bd
Төгөлдөр Намжилдорж <24LP5320@G401-CC0091.local>
2024-08-29 12:09:00 +0800

    class fux

--- src/components/ManagingState/ImmerAvoidDeeplyNestedState.jsx
+++ src/components/ManagingState/ImmerAvoidDeeplyNestedState.jsx
@@ -2,9 +2,11 @@ import { useImmer } from "use-immer"
 import { initialTravelPlan } from "./places"
 export const ImmerAvoidDeeplyNestedState=()=>{
     const root =initialTravelPlan[0]
-    const planetChilds=root.childIds
+    const planetChildrenIds=root.childIds
+    console.log('planet children ids:',planetChildrenIds)
     const [plan ,updatePlan]=useImmer(initialTravelPlan)
-    const handleComplete=({parentId,childId})=>{
+
+    const handleComplete=(parentId,childId)=>{
     updatePlan(draft=>{
     const parent=draft[parentId]
     parent.childIds=parent.childIds.filter(id=>id!==childId)
@@ -16,9 +18,24 @@ export const ImmerAvoidDeeplyNestedState=()=>{
     }
     })
     }
-    const PlaceTree =()=>{
-     
+    const PlaceTree =({id,placesById,parendId,onComplete})=>{
+     const place=placesById[id]
+     console.log('place is:',place)
+     const childrenIds=place.childIds 
+     return <li className="relative left-5">
+      {place.title}
+      <button className="ring-1 ml-2" onClick={()=>{
+        onComplete(parendId,id)
+      }} >Complete</button>
+      {childrenIds.length>0 && (<ol className="list-inside list-decimal">
+        {childrenIds.map(childId=><PlaceTree key={childId} id={childId} placesById={placesById} parendId={id} onComplete={onComplete}/>)}
+        </ol>)}
+     </li>
     }
     
-    return 
+    return (<ol className="list-inside list-decimal ">
+        {planetChildrenIds.map(planetChildId=>(<PlaceTree key={planetChildId} id={planetChildId} placesById={plan} parendId={0} onComplete={handleComplete}/>))}
+    </ol>)
+    
 }
+
