03ac358ff0dae15738c25aa6c16fd4ac1d4bd9fb
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-10-20 09:59:04 +0800

    not a change

---
+++ src/components/Hooks/01-use-transition/useTransition.jsx
@@ -0,0 +1,5 @@
+import { startTransition, useTransition } from "react"
+export const useTransition=()=>{
+cosnt [isPending,startTransition]=useTransition()
+
+}
---
+++ src/pages/hooks/index.jsx
@@ -0,0 +1,7 @@
+const Page=()=>{
+    return (
+        <div>hooks section</div>
+    )
+
+}
+export default Page
